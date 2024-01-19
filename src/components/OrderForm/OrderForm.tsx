527237a3ae6fa97b26ec0b22b0c4c174d1b9fa29
Julia_Grishina <julia9174@mail.ru>
2024-01-19 19:04:11 +0300

    form position done

--- src/components/OrderForm/OrderForm.tsx
+++ src/components/OrderForm/OrderForm.tsx
@@ -4,37 +4,40 @@ import Styles from './OrderForm.module.css';
 const OrderForm = () => {
     return(
         <>
-            <section className={Styles.form__conteiner_section}>
-                <div className={Styles.form__borders}>
-                    <form>
-                        <div>
-                            <p className={`${Styles.flowerLogoWhite} flowerLogo`}>Flower Fraught Karaganda</p>
-                            <h1>Заказать звонок</h1>
-                            <p className="text_normal_itallic">Закажите звонок и наш менеджер свяжется с вами в близжайшее время</p>
-                        </div>
-                        <div>
-                            <input type="text" placeholder="Имя"></input>
-                        </div>
-                        <div>
-                            <input type="phone" placeholder="Номер телефона"/>
-                        </div>
+            <section className={Styles.form__container_section}>
+                <div className={Styles.borders}>
+                    <div className={Styles.form__container}>
+                        <form>
+                            <div className={Styles.form__container_bg}>
+                                <div className={Styles.form__container_paddings}>
+                                    <div className={Styles.form__borders}>
+                                        <div className={Styles.form__text_container}>
+                                            <p className={`${Styles.flowerLogoWhite} flowerLogo`}>Flower Fraught Karaganda</p>
+                                            <h1 className={Styles.form__text_header}>Заказать звонок</h1>
+                                            <p className={`${Styles.form_textWhite} text_normal_itallic`}>Закажите звонок и наш менеджер свяжется с вами в близжайшее время</p>
+                                        </div>
+                                        <div className={Styles.form__container_inputs}>
+                                            <input type="text" placeholder="Имя"></input>
+                                        </div>
+                                        <div>
+                                            <input type="phone" placeholder="Номер телефона"/>
+                                        </div>
 
-                        <div>
-                            <input type="checkbox"/>
-                            <label className="text_normal_itallic">Даю согласие на обработку <span>персональных данных</span></label>
-                        </div>
-                        <div>
-                            <button className="text_button">Заказать звонок</button>
-                        </div>
-                    </form>
-                    <div className={Styles.form__pict_container}>
-                        <picture>
-
-                        </picture>
-
-                        <picture>
-                            
-                        </picture>
+                                        <div>
+                                            <input type="checkbox"/>
+                                            <label className="text_normal_itallic">Даю согласие на обработку <span>персональных данных</span></label>
+                                        </div>
+                                        <div className={Styles.form__container_button}>
+                                            <button className="text_button">Заказать звонок</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                        <div className={Styles.form__pict_container}>
+                            <div className={Styles.pict1}></div>
+                            <div className={Styles.pict2}></div>
+                        </div> 
                     </div>
                 </div>
             </section>

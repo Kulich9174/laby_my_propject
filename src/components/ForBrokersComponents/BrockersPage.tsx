35c8f476854a9f7d6355869de35e3853c08713fe
Julia_Grishina <julia9174@mail.ru>
2024-01-22 21:40:55 +0300

    ///

---
+++ src/components/ForBrokersComponents/BrockersPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Styles from './BrockersPage.module.css';
+
+const BrockersPage = () => {
+    return(
+        <>
+        
+        </>
+    )
+}
+export default BrockersPage;
--- src/pages/Contacts/Contacts.tsx
+++ src/pages/Contacts/Contacts.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import MainPageOtherPaths from "../../components/MainPageOnOtherPaths/ForBrokersMainPage/MainPageUnique";
+import OrderForm from "../../components/MainPageComponents/OrderForm/OrderForm";
 
 const Contacts = () => {
     return(
         <>
             <div>
                 <MainPageOtherPaths/>
+                <OrderForm/>
             </div>
         </>
     )

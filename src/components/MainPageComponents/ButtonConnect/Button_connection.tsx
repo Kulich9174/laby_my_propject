943c2b8a8759cbda919274239ce03ecb03d3a071
Julia_Grishina <julia9174@mail.ru>
2024-01-27 17:03:34 +0300

    button

--- src/components/MainPageComponents/ButtonConnect/Button_connection.tsx
+++ src/components/MainPageComponents/ButtonConnect/Button_connection.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Styles from './Button.module.css';
-import button_corner_left from '../../../assets/images/left_button_corner.svg';
-import button_corner_right from '../../../assets/images/right_button_corner.svg';
+
 
 export default function Button (){
 
@@ -9,8 +8,10 @@ export default function Button (){
         <>
         <button className={Styles.button_size}>
             <div className={Styles.button_input}>
-                {/* <img src={button_corner_left} alt="left corner" className={Styles.button_leftCorner}/> */}
+                <div className={Styles.border_top}></div>
                 <p className={Styles.button_text}>Связаться с нами</p>
+                <div className={Styles.border_bottom}></div>
+                {/* <img src={button_corner_left} alt="left corner" className={Styles.button_leftCorner}/> */}
                 {/* <img src={button_corner_right} alt="right corner" className={Styles.button_rightCorner}/> */}
             </div>
         </button>

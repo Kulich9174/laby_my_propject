6fc8e52f077a1af598c8f8990f0bc28b82d11126
JuliaGrishina <julia9174@mail.ru>
2024-02-07 18:34:49 +0300

    smooth scroll footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,13 +11,21 @@ const Footer: React.FC = () => {
     const headerstatevalue = useSelector((state: RootState)=>state.headerMenuState.value);
     const {t} = useTranslation();
     const langState = useSelector((state: RootState) => state.LangStateReducer.language);
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth' // Для плавного скролла
+        });
+    };
+
     return ( 
         <>
             <footer className="bg-[#1A2633] flex-grow-[1] m-0">
                 <div className=' text-[#BCC9DA] sm:grid sm:grid-rows-1 min-h-[100hv] sm:py-[30px] sm:pb-100hv sm:px-[14px] laptop:py-[47px] laptop:px-[34px] desktop:py-[40px]'>
                 <div className= {headerstatevalue ? 'sm:flex sm:text-center flex flex-wrap sm:justify-center items-center desktop:justify-center': "flex flex-wrap sm:justify-between items-center desktop:justify-center"}>
                 <div className={ headerstatevalue ? 'sm:hidden flex items-center sm:pb-[22px] pr-[5%]':'flex items-center sm:pb-[22px] pr-[5%]'}>
-                    <a href='#' className='pr-2'><img src={Logo} alt='logo' className='min-w-[30px] max-w-56px min-h-[30px] max-h-56px ' /></a>
+                    <a href='#' className='pr-2' onClick={scrollToTop}><img src={Logo} alt='logo' className='min-w-[30px] max-w-56px min-h-[30px] max-h-56px ' /></a>
                     <div className='text-[#FFFFFF] text-sm pb-0.5 font-bold pt-2 '>
                         <p className='pb-0.5'>Flower</p>
                         <p className='pb-0.5'>Fracht</p>
@@ -30,18 +38,18 @@ const Footer: React.FC = () => {
                     <ul className={ headerstatevalue ? 'sm:hidden flex flex-wrap justify-center -mx-2 laptop:items-center uppercase':"uppercase flex flex-wrap justify-center -mx-2 laptop:items-center"}>
                         {/* Первая колонка */}
                         <li className="w-1/2 px-2">
-                            <Link to="/" className="inline-block py-2 hover:text-[#227CE4]">{t('main')}</Link>
+                            <Link to="/" className="inline-block py-2 hover:text-[#227CE4]" onClick={scrollToTop}>{t('main')}</Link>
                         </li>
                         <li className="w-1/2 px-2">
-                            <Link to="/warehouses-page" className="inline-block py-2 hover:text-[#227CE4]">{t('our_warehouses')}</Link>
+                            <Link to="/warehouses-page" className="inline-block py-2 hover:text-[#227CE4]" onClick={scrollToTop}>{t('our_warehouses')}</Link>
                         </li>
 
                         {/* Вторая колонка */}
                         <li className="w-1/2 px-2">
-                            <Link to="/brockers-page" className="inline-block py-2 hover:text-[#227CE4]">{t('brockers')}</Link>
+                            <Link to="/brockers-page" className="inline-block py-2 hover:text-[#227CE4]" onClick={scrollToTop}>{t('brockers')}</Link>
                         </li>
                         <li className="w-1/2 px-2">
-                            <Link to="/contacts" className="inline-block py-2 hover:text-[#227CE4]">{t('contacts')}</Link>
+                            <Link to="/contacts" className="inline-block py-2 hover:text-[#227CE4]" onClick={scrollToTop}>{t('contacts')}</Link>
                         </li>
                     </ul>
                 </nav>
@@ -58,7 +66,7 @@ const Footer: React.FC = () => {
 
                 <div className={headerstatevalue ? 'relative sm:block flex flex-col lg:items-end justify-center items-start pt-[15px]': "flex flex-col lg:items-end justify-center items-start pt-[15px]"}>
                     <div className="mb-2 sm:pb-[8px]">
-                        <a href="#" className="text-[#BCC9DA] hover:text-[#227CE4]">{t('privat_policy')}</a>
+                        <a href="#" className="text-[#BCC9DA] hover:text-[#227CE4]" onClick={scrollToTop}>{t('privat_policy')}</a>
                     </div>
                     <div className="mb-2">
                         <a href="#" className="text-[#BCC9DA] hover:text-[#227CE4]">Copyright © 2021 All rights reserved</a>

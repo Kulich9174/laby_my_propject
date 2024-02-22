be7c9ef4c1411f558542915aa3384eb6bfc3d8e5
Julia_Grishina <julia9174@mail.ru>
2024-02-22 18:25:44 +0300

    tt

--- src/components/Warehouses/WarwhouseComponent.tsx
+++ src/components/Warehouses/WarwhouseComponent.tsx
@@ -26,20 +26,21 @@ const WarehouseComponent = () =>{
             <section className={Style.warehouse_section}>
                 <div className={Style.warehouse__paddingContainer}>
                     <div className={Style.warehouse_border}>
-                        <div className={Style.warehouse_countryContainer}>
-                            <div className={Style.countryList}>
-                                <button onClick={ ()=>handleClickCountry('country_Kz')} className={Style.warehouse__button}>{t('country_Kz')}</button>
-                                <button onClick={ ()=>handleClickCountry('country_Russia')} className={Style.warehouse__button}>{t('country_Russia')}</button>
-                                <button onClick={ ()=>handleClickCountry('country_Uzbek')} className={Style.warehouse__button}>{t('country_Uzbek')}</button>
-                                <button onClick={ ()=>handleClickCountry('country_Azer')} className={Style.warehouse__button}>{t('country_Azer')}</button>
-                                <button onClick={ ()=>handleClickCountry('country_Turkmenistan')} className={Style.warehouse__button}>{t('country_Turkmenistan')}</button>
-                                <button onClick={ ()=>handleClickCountry('country_Armenia')} className={Style.warehouse__button}>{t('country_Armenia')}</button>
-                                <button onClick={ ()=>handleClickCountry('country_Belorussia')} className={Style.warehouse__button}>{t('country_Belorussia')}</button>
+                        <div className={Style.warehouse__content_container}>
+                            <div className={Style.warehouse_countryContainer}>
+                                <div className={Style.countryList}>
+                                    <button onClick={ ()=>handleClickCountry('country_Kz')} className={Style.warehouse__button}>{t('country_Kz')}</button>
+                                    <button onClick={ ()=>handleClickCountry('country_Russia')} className={Style.warehouse__button}>{t('country_Russia')}</button>
+                                    <button onClick={ ()=>handleClickCountry('country_Uzbek')} className={Style.warehouse__button}>{t('country_Uzbek')}</button>
+                                    <button onClick={ ()=>handleClickCountry('country_Azer')} className={Style.warehouse__button}>{t('country_Azer')}</button>
+                                    <button onClick={ ()=>handleClickCountry('country_Turkmenistan')} className={Style.warehouse__button}>{t('country_Turkmenistan')}</button>
+                                    <button onClick={ ()=>handleClickCountry('country_Armenia')} className={Style.warehouse__button}>{t('country_Armenia')}</button>
+                                    <button onClick={ ()=>handleClickCountry('country_Belorussia')} className={Style.warehouse__button}>{t('country_Belorussia')}</button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                
             </section>
         </>
     )
